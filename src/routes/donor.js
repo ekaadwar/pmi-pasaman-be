0398c1db4d3c18d1364441cb11e41320736171aa
ekaadwar <eka.adwar@gmail.com>
2022-09-16 19:21:33 +0700

    menyelesaikan flow get donor data

--- src/routes/donor.js
+++ src/routes/donor.js
@@ -2,7 +2,7 @@ const route = require("express").Router();
 const donorController = require("../controllers/donor");
 const auth = require("../middlewares/auth");
 
-// route.get();
+route.get("/", auth, donorController.getDonorData);
 route.post("/", auth, donorController.addDonor);
 
 module.exports = route;
